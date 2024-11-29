c019b0058a2a73e54ff035f3fc6ce8346a7e6802
ahmetyagiz <ahmetanteryagiz@gmail.com>
2024-11-29 22:24:04 +0300

    JDBC-Intro

--- Repository-Pattern/src/Main.java
+++ Repository-Pattern/src/Main.java
@@ -2,6 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        Validator validator = new Validator();
+        Customer customer = new Customer();
+        validator.validate(customer);
     }
 }
