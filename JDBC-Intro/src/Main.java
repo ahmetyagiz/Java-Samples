16a58218aab55450d5685203369a10634ef00de9
ahmetyagiz <ahmetanteryagiz@gmail.com>
2024-11-30 12:17:09 +0300

    JBDC-Intro 2

--- JDBC-Intro/src/Main.java
+++ JDBC-Intro/src/Main.java
@@ -1,24 +1,21 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main
 {
-    static String userName = "root";
-    static String password = "12345";
-    static String dbUrl = "jdbc:mysql://localhost:3306/world";
-
     public static void main(String[] args) throws SQLException
     {
         Connection connection = null;
+        DbHelper helper = new DbHelper();
+
         try
         {
-            connection = DriverManager.getConnection(dbUrl, userName, password);
+            connection = helper.getConnection();
             System.out.println("Connected to database");
         }
         catch (SQLException exception)
         {
-            System.out.println(exception.getMessage());
+            helper.showErrorMessage(exception);
         }
         finally
         {
