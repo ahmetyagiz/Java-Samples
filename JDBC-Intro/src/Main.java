f4b58b28ee01335c8f220592d70edf4b0da1c1c8
ahmetyagiz <ahmetanteryagiz@gmail.com>
2024-11-30 15:32:28 +0300

    JDBC-Intro 3

--- JDBC-Intro/src/Main.java
+++ JDBC-Intro/src/Main.java
@@ -1,17 +1,27 @@
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class Main
 {
     public static void main(String[] args) throws SQLException
     {
+
+    }
+
+    private static void deleteDemo() throws SQLException {
         Connection connection = null;
         DbHelper helper = new DbHelper();
+
+        PreparedStatement statement = null;
+        ResultSet resultSet;
 
         try
         {
             connection = helper.getConnection();
-            System.out.println("Connected to database");
+            String sql = "delete from city where id = 4080";
+            statement = connection.prepareStatement(sql);
+            statement.executeUpdate();
+            System.out.println("Kayıt silindi");
         }
         catch (SQLException exception)
         {
@@ -19,6 +29,99 @@ public class Main
         }
         finally
         {
+            statement.close();
+            connection.close();
+        }
+    }
+
+    private static void updateDemo() throws SQLException {
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+
+        PreparedStatement statement = null;
+        ResultSet resultSet;
+
+        try
+        {
+            connection = helper.getConnection();
+            String sql = "update city set Population = 80000, district = 'Düzce' where id = 4080";
+            statement = connection.prepareStatement(sql);
+            statement.executeUpdate();
+            System.out.println("Kayıt güncellendi");
+        }
+        catch (SQLException exception)
+        {
+            helper.showErrorMessage(exception);
+        }
+        finally
+        {
+            statement.close();
+            connection.close();
+        }
+    }
+
+    private static void insertDemo() throws SQLException {
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+
+        PreparedStatement statement = null;
+        ResultSet resultSet;
+
+        try
+        {
+            connection = helper.getConnection();
+            String sql = "insert into city (Name, CountryCode, District, Population)values (?, ?, ?, ?)";
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, "Düzce");
+            statement.setString(2, "TUR");
+            statement.setString(3, "Turkey");
+            statement.setInt(4, 70000);
+            statement.executeUpdate();
+            System.out.println("Kayıt eklendi");
+        }
+        catch (SQLException exception)
+        {
+            helper.showErrorMessage(exception);
+        }
+        finally
+        {
+            statement.close();
+            connection.close();
+        }
+    }
+
+    private static void selectDemo() throws SQLException {
+        Connection connection = null;
+        DbHelper helper = new DbHelper();
+
+        Statement statement = null;
+        ResultSet resultSet;
+
+        try
+        {
+            connection = helper.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select Code,Name,Continent,Region from country ");
+            ArrayList<Country> countries = new ArrayList<Country>();
+
+            while (resultSet.next())
+            {
+                countries.add(new Country(
+                        resultSet.getString("Code"),
+                        resultSet.getString("Name"),
+                        resultSet.getString("Continent"),
+                        resultSet.getString("Region"))
+                );
+            }
+            System.out.println(countries.size());
+        }
+        catch (SQLException exception)
+        {
+            helper.showErrorMessage(exception);
+        }
+        finally
+        {
+            statement.close();
             connection.close();
         }
     }
