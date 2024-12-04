9a463a7c4309812eb2c30713825a279c9ccf8356
ahmetyagiz <ahmetanteryagiz@gmail.com>
2024-12-05 00:05:10 +0300

    tax-calculator

---
+++ tax-calculator/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int gross = sc.nextInt();
+
+        double tax = ((double) (gross * 18) / 100);
+        double taxedValue = gross + tax;
+
+        System.out.println("Gross: " + gross);
+        System.out.println("Taxed Value: " + taxedValue);
+        System.out.println("Tax: " + tax);
+    }
+}
