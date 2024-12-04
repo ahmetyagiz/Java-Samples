a9d04b4d602e09068c5bfe6d37f9fc30c0df20e4
ahmetyagiz <ahmetanteryagiz@gmail.com>
2024-12-04 14:49:01 +0300

    hibernateMapping

---
+++ hibernateDemo/src/hibernateDemo/City.java
@@ -0,0 +1,54 @@
+package hibernateDemo;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="city")
+public class City {
+	@Id
+	@Column(name="ID")
+	private String id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="countryCode")
+	private String countryCode;
+	
+	@Column(name="district")
+	private String district;
+	
+	@Column(name="population")
+	private int population;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountryCode() {
+		return countryCode;
+	}
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public int getPopulation() {
+		return population;
+	}
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+}
