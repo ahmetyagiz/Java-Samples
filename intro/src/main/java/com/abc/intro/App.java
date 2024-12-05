aac586f7a9048a31cda6d585bde3d4e441826e02
ahmetyagiz <ahmetanteryagiz@gmail.com>
2024-12-05 13:31:28 +0300

    Maven

--- intro/src/main/java/com/abc/intro/App.java
+++ intro/src/main/java/com/abc/intro/App.java
@@ -2,9 +2,12 @@ package com.abc.intro;
 
 /**
  * Hello world!
+ *
  */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
