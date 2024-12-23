3d93752b6ee0f61634a7d29c98e3757e326faa71
ahmetyagiz <ahmetanteryagiz@gmail.com>
2024-12-23 16:27:12 +0300

    spring-data-jpa: Validation

--- spring-data-jpa/src/main/java/com/ahmetyagiz/dto/DtoStudent.java
+++ spring-data-jpa/src/main/java/com/ahmetyagiz/dto/DtoStudent.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class DtoStudent {
 	//GET
-	
+
 	private String firstName;
 	
 	private String lastName;
--- spring-data-jpa/src/main/java/com/ahmetyagiz/dto/DtoStudentIU.java
+++ spring-data-jpa/src/main/java/com/ahmetyagiz/dto/DtoStudentIU.java
@@ -2,6 +2,9 @@ package com.ahmetyagiz.dto;
 
 import java.sql.Date;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +16,20 @@ import lombok.Setter;
 @AllArgsConstructor
 public class DtoStudentIU {
 	//SET
+	@NotEmpty(message = "Firstname alanı boş bırakılamaz!")
 	
+	@Size(min = 3, max = 30)
 	private String firstName;
 	
+	@Size(min = 3, max = 30)
 	private String lastName;
 	
 	private Date birthOfDate;
+	
+	@Email(message = "Email formatında bir adres giriniz!")
+	private String email;
+	
+	@Size(min = 11, max = 11)
+	@NotEmpty(message = "Tckn alanı boş geçilemez!")
+	private String tckn;
 }
