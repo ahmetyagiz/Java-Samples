5e8c92084d83bfb664935897c9f42e8e1eabfa68
ahmetyagiz <ahmetanteryagiz@gmail.com>
2025-03-10 19:58:32 +0300

    Update Reverse Integer

--- reverse-integer/src/Main.java
+++ reverse-integer/src/Main.java
@@ -22,30 +22,47 @@ class Main {
         System.out.println(reverse(-321));
     }
 
-    private static int reverse(int num) {
-        boolean isNumNegative = false;
+    private static int reverse(int x) {
+        int reversed = 0;
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        while (x != 0) {
+            int lastDigit = x % 10;
 
-        if(num < 0){
-            isNumNegative = true;
-            num = Math.abs(num);
+            if (reversed > max / 10 || (reversed == max / 10 && lastDigit > 7))
+                return 0;
+            if (reversed < min / 10 || (reversed == min / 10 && lastDigit < -8))
+                return 0;
+            reversed = reversed * 10 + lastDigit;
+            x = x / 10;
         }
-
-        StringBuilder builder = new StringBuilder();
-
-        while(num != 0){
-            builder.append(num % 10);
-            num /= 10;
-        }
-
-        if(isNumNegative){
-            builder.insert(0, '-');
-        }
-        String str = builder.toString();
-
-        try{
-            return Integer.parseInt(str);
-        } catch (NumberFormatException ex) {
-            return 0;
-        }
+        return reversed;
     }
+
+//    private static int reverse(int num) {
+//        boolean isNumNegative = false;
+//
+//        if (num < 0) {
+//            isNumNegative = true;
+//            num = Math.abs(num);
+//        }
+//
+//        StringBuilder builder = new StringBuilder();
+//
+//        while (num != 0) {
+//            builder.append(num % 10);
+//            num /= 10;
+//        }
+//
+//        if (isNumNegative) {
+//            builder.insert(0, '-');
+//        }
+//        String str = builder.toString();
+//
+//        try {
+//            return Integer.parseInt(str);
+//        } catch (NumberFormatException ex) {
+//            return 0;
+//        }
+//    }
 }
